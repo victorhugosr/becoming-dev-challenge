df405f5214ecb74ccc526d8ac8d06a9a80b660a7
Victor Hugo <victorhugosr77@gmail.com>
2023-09-19 19:35:19 -0300

    add button functions

--- js/index.js
+++ js/index.js
@@ -7,4 +7,31 @@ function updateChallengeText(){
     document.getElementById('challenge-text').textContent = `${formattedDate} - Day ${dayCounter} of the challenge`;
 }
 
+function toggleChallenge() {
+    const button = document.getElementById('challenge-button');
+
+    if (!challengeActive) {
+        challengeActive = true;
+        button.textContent = 'CANCEL CHALLENGE'
+        document.getElementById('challenge-status').textContent = 'Pressione para cancelar o desafio';
+
+        dayCounter = 1;
+        updateChallengeText();
+
+        challengeInterval = setInterval(function (){
+            dayCounter++;
+            updateChallengeText();
+        }, 86400000); //86400000 é a quantidade de milissegundos em um dia.
+    } else {
+        challengeActive = false;
+        button.textContent = 'START CHALLENGE';
+        document.getElementById('challenge-status').textContent = 'Pressione para começar o desafio';
+
+        clearInterval(challengeInterval);
+        dayCounter = 0;
+        updateChallengeText();
+    }
+
+}
+
 updateChallengeText();
--- js/pt-br_notes_for_beginners.md
+++ js/pt-br_notes_for_beginners.md
@@ -21,4 +21,22 @@ function dataAtualizadaDesafio(){
 >>```document.getElementById``` // Método JS usado para pegar uma referência do seu documento HTML
 >>```toLocaleDateString();``` //Método JS usado para converter o objeto data para o formato de data localizada no navegador do usuário.
 >>>```textContent``` é uma propriedade de elementos HTML que permite acessar e modificar o conteúdo de texto dentro desse elemento
+
 [Clique aqui para ver mais sobre o Date](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Date)
+
+---
+>>Agora eu quero que o botão mude a frase dentro dele 'START CHALLENGE' para 'CANCEL CHALLENGE' após ser clicado também que ele exiba uma mensagem chamando o usuário para a ação após ele passar o mouse
+
+### Como faço pra mudar
+vamos usar as condições!
+primeiro chamamos o id do Botão, ques está no HTML.
+
+```
+function mudeoDesafio(){
+
+    const button = document.getElementById('desafio-botao');
+
+    if ()
+}
+
+```
