a3e46c2470a9d8e336410cd9b327b752921366bf
Victor Hugo <victorhugosr77@gmail.com>
2023-10-04 21:19:48 -0300

    add js challenge - days status

--- js/index.js
+++ js/index.js
@@ -1,60 +1,101 @@
-let currentDate = new Date();
-let dayCounter = 0;
-let challengeActive = false
-let challengeInterval;
+const totalDays = 210;
+const datesData = window.localStorage.getItem('datesData') || {}
+const dayContainer = document.querySelector('#days-container')
 
-function setCookie(name, value){
-    document.cookie = `${name} = ${value};path=/`;
+function renderSquare(day, container) {
+    const squareElement = document.createElement('div')
+    squareElement.classList.add('day')
+    squareElement.setAttribute('id', day.date)
+
+    if (day.hasStudied && day.hasPractice) {
+        squareElement.classList.add('complete-day')
+    } else if (day.hasStudied || day.hasPractice) {
+        squareElement.classList.add('half-day')
+    }
+    container.appendChild(squareElement)
 }
 
-function getCookie(name) {
-    const cookies = document.cookie.split('; ');
-    for (const cookie of cookies) {
-        const [cookieName, cookieValue] = cookie.split('=');
-        if (cookieName === name) {
-            return cookieValue;
+function renderSquares(startDay, totalMonths, dayContainer) {
+    // data atual
+    let currentDate = dayjs(startDay)
+    // data final do desafio
+    let endDate = currentDate.add(totalMonths, 'days')
+
+    while (currentDate.isBefore(endDate)) {
+        // obtém a data atual no formato yyyy-mm-dd
+        const dateAsIso = currentDate.toISOString().slice(0, 10);
+        // dado do dia
+        const dayData = datesData[dateAsIso]
+        renderSquare(dayData || {date: dateAsIso}, dayContainer)
+
+        currentDate = currentDate.add(1, 'day');
+    }
+}
+
+function startChallenge() {
+    dayContainer.innerHTML = ''
+    if (!window.localStorage.getItem('startDay')) {
+        window.localStorage.setItem('startDay', new Date().toISOString().slice(0, 10))
+    }
+
+    const startDay = dayjs(window.localStorage.getItem('startDay'))
+    renderSquares(startDay, totalDays, dayContainer)
+}
+
+(() => {
+    if(window.localStorage.getItem('startDay')){
+        startChallenge()
+        return
+    }
+
+    renderFakeDays()
+})()
+
+function renderFakeDays(){
+    for(i = 0; i < totalDays; i++){
+        renderSquare({date: dayjs().add(i, 'days').toISOString()},  dayContainer)
+    }
+}
+
+function updateDayData(date, key, value){
+    const daysData = getDayData()
+    daysData[date][key] = value
+    localStorage.setItem('datesData', JSON.stringify(daysData))
+}
+
+function makeDayAsStudied(){
+    const today = new Date().toISOString().slice(0, 10)
+    updateDayData(today, 'hasStudied', true)
+    updateSquareStyle(today)
+}
+
+function makeDayAsPractice(){
+    const today = new Date().toISOString().slice(0, 10)
+    updateDayData(today, 'hasPractice', true)
+    updateSquareStyle(today)
+}
+
+function getDayData(date){
+    const daysData = JSON.parse(localStorage.getItem('datesData') || {})
+    if(!date){
+        return daysData
+    }
+
+    if(!daysData[date]){
+        daysData[date] = {
+            date: date,
         }
     }
+    return daysData[date]
 }
 
-function updateChallengeText(){
-    const formattedDate = currentDate.toLocaleDateString();
-    document.getElementById('challenge-text').textContent = `${formattedDate} - Day ${dayCounter} of the challenge`;
-}
-
-function toggleChallenge() {
-    const button = document.getElementById('challenge-button');
-
-    if (!challengeActive) {
-        challengeActive = true;
-        button.textContent = 'CANCEL CHALLENGE'
-        document.getElementById('challenge-status').textContent = 'Pressione para cancelar o desafio';
-
-        dayCounter = 1;
-        updateChallengeText();
-
-        challengeInterval = setInterval(function (){
-            dayCounter++;
-            updateChallengeText();
-        }, 86400000); //86400000 é a quantidade de milissegundos em um dia.
-
-        setCookie('challengeActive', 'true', 365);
-    } else {
-        challengeActive = false;
-        button.textContent = 'START CHALLENGE';
-        document.getElementById('challenge-status').textContent = 'Pressione para começar o desafio';
-
-        clearInterval(challengeInterval);
-        dayCounter = 0;
-        updateChallengeText();
-
-        setCookie('challengeActive', '', -1);
+function updateSquareStyle(date){
+    const squareElement = document.getElementById(date)
+    const dayData = getDayData(date)
+    
+    if (day.hasStudied && day.hasPractice) {
+        squareElement.classList.add('complete-day')
+    } else if (day.hasStudied || day.hasPractice) {
+        squareElement.classList.add('half-day')
     }
 }
-
-const challengeCookie = getCookie('challengeActive');
-if (challengeCookie === 'true') {
-    toggleChallenge();
-}
-
-updateChallengeText();
--- js/pt-br_notes_for_beginners.md
+++ js/pt-br_notes_for_beginners.md
@@ -40,3 +40,65 @@ function mudeoDesafio(){
 }
 
 ```
+
+
+let currentDate = new Date();
+let dayCounter = 0;
+let challengeActive = false
+let challengeInterval;
+
+function setCookie(name, value){
+    document.cookie = `${name} = ${value};path=/`;
+}
+
+function getCookie(name) {
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split('=');
+        if (cookieName === name) {
+            return cookieValue;
+        }
+    }
+}
+
+function updateChallengeText(){
+    const formattedDate = currentDate.toLocaleDateString();
+    document.getElementById('challenge-text').textContent = `${formattedDate} - Day ${dayCounter} of the challenge`;
+}
+
+function toggleChallenge() {
+    const button = document.getElementById('challenge-button');
+
+    if (!challengeActive) {
+        challengeActive = true;
+        button.textContent = 'CANCEL CHALLENGE'
+        document.getElementById('challenge-status').textContent = 'Pressione para cancelar o desafio';
+
+        dayCounter = 1;
+        updateChallengeText();
+
+        challengeInterval = setInterval(function (){
+            dayCounter++;
+            updateChallengeText();
+        }, 86400000); //86400000 é a quantidade de milissegundos em um dia.
+
+        setCookie('challengeActive', 'true', 365);
+    } else {
+        challengeActive = false;
+        button.textContent = 'START CHALLENGE';
+        document.getElementById('challenge-status').textContent = 'Pressione para começar o desafio';
+
+        clearInterval(challengeInterval);
+        dayCounter = 0;
+        updateChallengeText();
+
+        setCookie('challengeActive', '', -1);
+    }
+}
+
+const challengeCookie = getCookie('challengeActive');
+if (challengeCookie === 'true') {
+    toggleChallenge();
+}
+
+updateChallengeText();
