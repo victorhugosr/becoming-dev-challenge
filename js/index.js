b863bc9f9c82d8e93b152352c50d4ed366fa212b
Victor Hugo <victorhugosr77@gmail.com>
2023-10-07 18:48:18 -0300

    Commenting Code

--- js/index.js
+++ js/index.js
@@ -1,30 +1,42 @@
+// Definição do número total de dias do desafio
 const totalDays = 210;
+
+// Recuperação dos dados das datas armazenadas no localStorage ou inicialização de um objeto vazio
 const datesData = window.localStorage.getItem('datesData') || {}
+
+// Seleciona o elemento com o ID "days-container" para posterior manipulação
 const dayContainer = document.querySelector('#days-container')
 
+// Função para rendereizar um quadrado no grid de acordo com o estado de cada dia
 function renderSquare(day, container) {
     const squareElement = document.createElement('div')
     squareElement.classList.add('day')
     squareElement.setAttribute('id', day.date)
 
+    // Define a classe CSS do quadrado com base no estado do dia
     if (day.hasStudied && day.hasPractice) {
         squareElement.classList.add('complete-day')
     } else if (day.hasStudied || day.hasPractice) {
         squareElement.classList.add('half-day')
     }
+
+    // Adiciona o quadrado ao contêiner
     container.appendChild(squareElement)
 }
 
+
+// Função para rendereizar os quadrados do grid
 function renderSquares(startDay, totalMonths, dayContainer) {
     // data atual
     let currentDate = dayjs(startDay)
     // data final do desafio
     let endDate = currentDate.add(totalMonths, 'days')
 
+    // Loop para criar e renderizar os quadrados de cada dia
     while (currentDate.isBefore(endDate)) {
         // obtém a data atual no formato yyyy-mm-dd
         const dateAsIso = currentDate.toISOString().slice(0, 10);
-        // dado do dia
+        // Obtém os dados do dia ou cria um objeto vazio
         const dayData = datesData[dateAsIso]
         renderSquare(dayData || {date: dateAsIso}, dayContainer)
 
